6e27d48279d8056947b95af07f1808aa5f5a028a
sevastosm <sevastos@aegeantaxi.com>
2019-10-22 13:26:08 +0300

    ADDS REST DATA

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-rest-api',
+      options: {
+        endpoints: [
+          'https://backenddev.thepearls.com/api/Web/Properties'
+        ],
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,51 @@
 import React from "react"
 import { Link } from "gatsby"
+import { graphql } from 'gatsby'
+
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
 
+  console.log("DATA", data)
+  const vilas = data.allRestApiApiWebProperties.edges
+  console.log("vilas", vilas)
+  return (
+
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <div>
+        <ul>
+          {vilas.map((item) => {
+            let newItem = item.next
+            if (newItem)
+              return <li>{JSON.stringify(newItem.title)}</li>
+          })}
+        </ul>
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>)
+}
+export const query = graphql`
+{
+  allRestApiApiWebProperties {
+    edges {
+      next {
+        id
+        title
+        description
+      }
+    }
+  }
+}
+`
 export default IndexPage
+
