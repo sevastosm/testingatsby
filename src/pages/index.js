f913724139bfb62633d63b6a68f37ce12483ca32
sevastosm <sevastos@aegeantaxi.com>
2019-10-22 13:30:58 +0300

    ww

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ const IndexPage = ({ data }) => {
 
     <Layout>
       <SEO title="Home" />
-      <h1>Hi people</h1>
+      <h1>Hi people ddd</h1>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
